65982fa7b0793c85170d8e504ee2328562d455e5
Shynet <shynet@gmail.com>
2019-09-30 18:06:29 +0300

    Removed useless assets

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    private Transform bar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar = transform.Find("Bar");
+    }
+
+    void SetHealth(float value)
+    {
+        bar.localScale = new Vector3(value, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
