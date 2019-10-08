1020044731f40d0e60d57c584c98d6bad8c519f6
Shynet <shynet@gmail.com>
2019-10-08 16:54:12 +0300

    Working on touch controls

--- Assets/Scripts/Player/TouchControls.cs
+++ Assets/Scripts/Player/TouchControls.cs
@@ -3,11 +3,12 @@
 public class TouchControls : MonoBehaviour
 {
     public CameraMovement cameraMovement;
+    public float movementSensitivity = 0.1f;
 
     // Update is called once per frame
     void Update()
     {
-
+        MoveCameraLogic();
     }
 
     /**
@@ -20,7 +21,7 @@ Moves the camera according  */
             if (touch.phase == TouchPhase.Moved)
             {
                 // Get movement of the finger since last frame
-                var touchDeltaPosition = touch.deltaPosition;
+                var touchDeltaPosition = touch.deltaPosition * movementSensitivity;
                 cameraMovement.MoveCamera(touchDeltaPosition.x, touchDeltaPosition.y);
             }
 
