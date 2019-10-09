c19ef1da335eb0d9a20df97512cade175a8124e3
Shynet <shynet@gmail.com>
2019-10-10 01:05:30 +0300

    Added touch buttons for mobile

---
+++ Assets/JumpButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JumpButton : MonoBehaviour, IPointerDownHandler
+{
+    public PlayerControl playerControl;
+    // Start is called before the first frame update
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        playerControl.Jump();
+    }
+}
