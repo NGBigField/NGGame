b6bd148996f694be2f704693d24bc60874d4c0fd
Shynet <shynet@gmail.com>
2019-10-10 23:23:34 +0300

    Minor add for gyro

---
+++ Assets/GyroControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This script moves the camera according to the device gyroscope sensor, if available.
+/// </summary>
+public class GyroControl : MonoBehaviour {
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+    private Quaternion rot;
+
+    private void Start () {
+        gyroEnabled = EnableGyro ();
+    }
+
+    private void Update () {
+        if (gyroEnabled) {
+            // TODO: Insert our update code according to the gyroscope location
+        }
+    }
+
+    private bool EnableGyro () {
+        if (SystemInfo.supportsGyroscope) {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            return true;
+        }
+
+        return false;
+    }
+}
--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,28 +1,24 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour
-{
+public class Inventory : MonoBehaviour {
     public PlayerCanvas playerCanvas;
 
-    public List<BaseInventoryItem> items = new List<BaseInventoryItem>();
+    public List<BaseInventoryItem> items = new List<BaseInventoryItem> ();
 
-    public void Reset()
-    {
+    public void Reset () {
         // Destory all of the items added
         foreach (var item in items)
-            Destroy(item);
+            Destroy (item);
     }
 
-    public BaseInventoryItem AddItem(System.Type itemType)
-    {
-        var item = (BaseInventoryItem)gameObject.AddComponent(itemType);
-        items.Add(item);
+    public BaseInventoryItem AddItem (System.Type itemType) {
+        var item = (BaseInventoryItem) gameObject.AddComponent (itemType);
+        items.Add (item);
         return item;
     }
 
-    public void OnItemDestroy(BaseInventoryItem item)
-    {
-        items.Remove(item);
+    public void OnItemDestroy (BaseInventoryItem item) {
+        items.Remove (item);
     }
 }
