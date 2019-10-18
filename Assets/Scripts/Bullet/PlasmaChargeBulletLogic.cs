cd539d08af9efca893727b2055dbf3b1745ca7c2
Shynet <shynet@gmail.com>
2019-10-18 23:13:30 +0300

    Working on plasma shoot animation

---
+++ Assets/Scripts/Bullet/PlasmaChargeBulletLogic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlasmaChargeBulletLogic : MonoBehaviour {
+    public const float MaxSize = 1f;
+
+    private float currentSize;
+
+    private float sizeIncValue = 0.05f;
+
+    // Start is called before the first frame update
+    void Start () {
+        // Take the start value from the local scale X value
+        currentSize = transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        currentSize = Mathf.Min (currentSize + (sizeIncValue * Time.deltaTime), MaxSize);
+        transform.localScale = new Vector3 (1, 1, 1) * currentSize;
+    }
+}
